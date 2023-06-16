1711e992dba76fa290679e73d48fa60ccc97f3a1
BuccaBunder5 <124277076+BuccaBunder5@users.noreply.github.com>
2023-06-16 11:43:17 +0200

    Crank that

--- bullet.c
+++ bullet.c
@@ -1,44 +1,132 @@
 /*
  * bullet.c
- *142
- *152
- *200
- *224
- *292
- *300
+ *
+ *  Created on: 14. jun. 2023
+ *      Author: danie
+ */
+/*
+ * bullet.c
  *  Created on: 13. jun. 2023
  *      Author: danie
  */
-#include "stdint.h"
-#include "stdio.h"
+#include "bullet.h"
 
-void Adder(vectorb* a, vectorb* b) {
-	a->x += b->x;
-	a->y += b->y;
-}
-void ShipPosRead(spaceship_t) {
-	(*spaceship).positionX + 0.2;
-	(*spaceship).positionY + 0.2;
+void SpawnBullet(bullet_t bullet[], int8_t scope) {
+	int8_t i;
+	for(i = 0; i<scope; i++) {
 
-}
-
-void ShipSpeedRead(spaceship_t) {
-	(*spaceship).vectorX + 1.5;
-	(*spaceship).vectorY + 1.5;
-
-}
-
-void ShootBullets(spaceship_t) {
-	int i = 0;
-	for(int i = 0; BULLETS; i++) {
-
-		if (spaceship_t->bullets[i].active == FALSE) {
-
-			spaceship_t->bullets[i].active = TRUE;
-			spaceship_t->bullets[i].position= ShipPosRead;
-			spaceship_t->bullets[i].bspeed = ShipSpeedRead;
-			break;
-		}
+	bullet[i].positionX = 0;
+	bullet[i].positionY = 0;
+	bullet[i].vectorX = 0;
+	bullet[i].vectorY = 0;
+	bullet[i].active = 0;
 	}
 }
 
+void Shoot(int8_t count,bullet_t  bullet[], spaceship_t * spaceship) {
+	int8_t i;
+	i = count;
+	bullet[i].positionX = (*spaceship).x;
+	bullet[i].positionY = (*spaceship).y;
+	bullet[i].active = 1;
+
+	switch((*spaceship).rotation){
+	case 0:
+		bullet[i].vectorX = 0;
+		bullet[i].vectorY = -128;
+		break;
+
+	case 1:
+		bullet[i].vectorX = 128;
+		bullet[i].vectorY = -128;
+		break;
+
+	case 2:
+		bullet[i].vectorX = 128;
+		bullet[i].vectorY = 0;
+		break;
+
+	case 3:
+		bullet[i].vectorX = 128;
+		bullet[i].vectorY = 128;
+		break;
+
+	case 4:
+		bullet[i].vectorX = 0;
+		bullet[i].vectorY = 128;
+		break;
+
+	case 5:
+		bullet[i].vectorX = -128;
+		bullet[i].vectorY = 128;
+		break;
+
+	case 6:
+		bullet[i].vectorX = -128;
+		bullet[i].vectorY = 0;
+		break;
+
+	case 7:
+		bullet[i].vectorX = -128;
+		bullet[i].vectorY = -128;
+		break;
+	}
+		gotoxy(bullet[i].positionX,bullet[i].positionY);
+		printf("*");
+
+}
+
+void moveBullet(bullet_t * bullet) {
+	(*bullet).nextPosX = (*bullet).positionX + (*bullet).vectorX;
+	if((*bullet).nextPosX > 128*190) {
+		(*bullet).nextPosX = 0;
+	} else if((*bullet).nextPosX < 0) {
+		(*bullet).nextPosX = 128*190;
+	}
+
+	(*bullet).nextPosY = (*bullet).positionY + (*bullet).vectorY;
+	if((*bullet).nextPosX > 128*55) {
+		(*bullet).nextPosX = 0;
+	} else if((*bullet).nextPosX < 0) {
+		(*bullet).nextPosX = 128*55;
+	}
+	gotoxy((*bullet).positionX, (*bullet).positionY);
+	printf(" ");
+	(*bullet).positionX = (*bullet).nextPosX;
+	(*bullet).positionY = (*bullet).nextPosY;
+	gotoxy((*bullet).positionX, (*bullet).positionY);
+	printf("*");
+ }
+
+
+/*void BulletDecay(bullet b[]) {
+		int32_t i;
+		for (i=0 ; i<8; i++){
+			if (!(b[i].active == 0)) {
+				int32_t positionX = b[i].positionX>>8;
+				int32_t positionY = b[i].positionY>>8;
+				b[i].positionX = b[i].positionX + b[i].vectorX;
+				b[i].positionY = b[i].positionY + b[i].vectorY;
+				int32_t nextPosX = b[i].positionX>>8;
+				int32_t nextPosY = b[i].positionY>>8;
+				if (nextPosY<grænse && nextPosY>grænse && nextPosX>grænse && nextPosX<grænse) {
+					gotoxy(x,y);
+					fgcolor(0);
+					printf("\x2A");
+					gotoxy(nextPosX, nextPosY);
+					fgcolor(15);
+					printf("\x2A");
+				}else {
+					gotoxy(x,y);
+					fgcolor(0);
+					printf("\x2A");
+					a[i].active = 0;
+					a[i].positionX = 0;
+					a[i].positionY = 0;
+					a[i].vectorX = 0;
+					a[i].vectorY = 0;
+				}
+			}
+		}
+	}
+*/
--- bullet.h
+++ bullet.h
@@ -4,30 +4,34 @@
  *  Created on: 13. jun. 2023
  *      Author: danie
  */
-#include
 
 #ifndef BULLET_H_
 #define BULLET_H_
 
-enum boolean {TRUE, FALSE};
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "math.h"
+#include "ansi.h"
 
 typedef struct {
-	struct vectorb position;
-	struct vectorb bulletspeed;
-	enum boolean primed;
-
-} bullet;
+	int32_t positionX;
+	int32_t positionY;
+	int32_t vectorX;
+	int32_t vectorY;
+	int32_t nextPosX;
+	int32_t nextPosY;
+	int8_t active;
+	int8_t counter;
+} bullet_t;
 
 typedef struct {
-    float x;
-	float y;
+	int32_t x, y, rotation, velX, velY, nextPosX, nextPosY;
+} spaceship_t ;
 
-} vectorb;
-
-
-void Adder(vectorb* a, vectorb* b);
-void ShipPosRead(spaceship_t);
-void ShipSpeedRead(spaceship_t);
-void ShootBullets(*spaceship, s);
-
+void SpawnBullet(bullet_t bullet[], int8_t scope);
+void Shoot(int8_t count,bullet_t  bullet[], spaceship_t * spaceship);
 #endif /* BULLET_H_ */
+
+
